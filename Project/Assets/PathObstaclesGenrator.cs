36732a11c35d93c01429c080af5741d36c4f9c65
Saleh Alsaleh <saleh.alsaleh@outlook.com>
2021-01-29 20:18:33 +0200

    path12

---
+++ Project/Assets/PathObstaclesGenrator.cs
@@ -0,0 +1,260 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathObstaclesGenrator : MonoBehaviour
+{
+
+    public GameObject[] pathsobjects;
+    public int pathLegnth = 10;
+    public GameObject goal;
+    public GameObject target;
+    public List<Transform> targetlocations = new List<Transform>();
+    public GameObject empty;
+    public GameObject[] Obstaclesobjects;
+    private int n = 0;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+
+
+
+    }
+
+
+    //public void CreatePath()
+
+    //{
+    //    //targetlocations = new Transform[pathLegnth + 1];
+    //    // Debug.Log(targetlocations.Length);
+
+
+
+
+    //    GameObject tmp;
+    //    tmp = Instantiate(empty);
+    //    tmp.transform.parent = transform;
+    //    tmp.transform.localPosition = new Vector3(0, 0, 0);
+    //    Transform tmp2 = transform;
+
+
+
+    //    //Debug.Log(Random.Range(0, pathsobjects.Length ));
+    //    tmp = Instantiate(pathsobjects[Random.Range(0, pathsobjects.Length)]);
+    //    tmp.transform.parent = transform;
+    //    tmp.transform.localPosition = tmp2.localPosition + tmp2.GetChild(0).localPosition;
+    //    tmp.transform.localRotation = Quaternion.Euler(0, 0, 0);
+    //    tmp.layer = 9;
+
+
+    //    targetlocations.Add(tmp2.GetChild(0).transform);
+
+    //    tmp2 = tmp.transform;
+
+    //    for (int objectindex = 1; objectindex < pathLegnth; objectindex++)
+    //    {
+
+
+    //        //Debug.Log(Random.Range(0, pathsobjects.Length ));
+    //        tmp = Instantiate(pathsobjects[Random.Range(0, pathsobjects.Length)]);
+    //        tmp.transform.parent = transform;
+    //        tmp.transform.localPosition = tmp2.localPosition + tmp2.GetChild(1).localPosition;
+    //        tmp.transform.localRotation = Quaternion.Euler(0, 0, 0);
+    //        tmp.layer = 9;
+
+
+    //        targetlocations.Add(tmp2.GetChild(1).transform);
+
+    //        tmp2 = tmp.transform;
+
+
+    //    }
+
+    //    tmp = Instantiate(goal);
+    //    tmp.transform.parent = transform;
+    //    tmp.transform.localPosition = tmp2.localPosition + tmp2.GetChild(1).localPosition + new Vector3(0, 0, 4);
+    //    tmp.transform.localRotation = Quaternion.Euler(0, 0, 0);
+    //    tmp.layer = 9;
+    //    targetlocations.Add(tmp2.GetChild(1).transform);
+
+
+    //    n = 0;
+
+    //    target.transform.position = targetlocations[n].position + new Vector3(0, 1, 0);
+
+    //    NextTarget();
+    //}
+    public void CreatePath()
+
+    {
+        //targetlocations = new Transform[pathLegnth + 1];
+        // Debug.Log(targetlocations.Length);
+
+
+
+
+        GameObject tmp;
+        GameObject obstacletmp;
+        tmp = Instantiate(empty);
+        tmp.transform.parent = transform;
+        tmp.transform.localPosition = new Vector3(0, 0, 0);
+        Transform tmp2 = transform;
+
+
+
+        //Debug.Log(Random.Range(0, pathsobjects.Length ));
+        tmp = Instantiate(pathsobjects[Random.Range(0, pathsobjects.Length)]);
+        tmp.transform.parent = transform;
+        tmp.transform.localPosition = tmp2.localPosition + tmp2.GetChild(0).localPosition;
+        tmp.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        tmp.layer = 9;
+
+
+        targetlocations.Add(tmp2.GetChild(0).transform);
+
+        tmp2 = tmp.transform;
+
+        int s = tmp.transform.childCount;
+        for (int k = 2; k < s; k++)
+        {
+            Transform child;
+            child = tmp.transform.GetChild(k);
+            obstacletmp = Instantiate(Obstaclesobjects[Random.Range(0, Obstaclesobjects.Length)],child);
+            obstacletmp.layer = 9;
+            //obstacletmp.transform.parent = tmp.transform;
+            //obstacletmp.transform.localPosition +=  child.localPosition;
+            //obstacletmp.transform.localRotation = child.localRotation;
+
+            //Debug.LogError(tmp.transform.childCount);
+            //Debug.Log(child.transform.localPosition);
+
+        }
+
+        for (int objectindex = 1; objectindex < pathLegnth; objectindex++)
+        {
+
+
+            //Debug.Log(Random.Range(0, pathsobjects.Length ));
+            tmp = Instantiate(pathsobjects[Random.Range(0, pathsobjects.Length)]);
+            tmp.transform.parent = transform;
+            tmp.transform.localPosition = tmp2.localPosition + tmp2.GetChild(1).localPosition;
+            tmp.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            tmp.layer = 9;
+
+
+            targetlocations.Add(tmp2.GetChild(1).transform);
+
+            tmp2 = tmp.transform;
+
+            s = tmp.transform.childCount;
+            for (int k = 2; k < s; k++)
+            {
+                Transform child;
+                child = tmp.transform.GetChild(k);
+                obstacletmp = Instantiate(Obstaclesobjects[Random.Range(0, Obstaclesobjects.Length)], child);
+                obstacletmp.layer = 9;
+                //obstacletmp.transform.parent = tmp.transform;
+                //obstacletmp.transform.localPosition += child.localPosition;
+                //obstacletmp.transform.localRotation = child.localRotation;
+
+                //Debug.LogError(tmp.transform.childCount);
+                //Debug.Log(child.transform.localPosition);
+
+            }
+
+
+        }
+
+        tmp = Instantiate(goal);
+        tmp.transform.parent = transform;
+        tmp.transform.localPosition = tmp2.localPosition + tmp2.GetChild(1).localPosition + new Vector3(0, 0, 4);
+        tmp.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        tmp.layer = 9;
+        targetlocations.Add(tmp2.GetChild(1).transform);
+
+
+        n = 0;
+
+        target.transform.position = targetlocations[n].position + new Vector3(0, 1, 0);
+
+        NextTarget();
+    }
+
+
+
+    public void DestroyPath()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        targetlocations.Clear();
+
+
+
+
+    }
+
+
+    public bool NextTarget()
+    {
+
+        n++;
+
+        if (n == targetlocations.Count)
+        {
+            return true;
+        }
+        else
+        {
+            target.transform.position = targetlocations[n].position + new Vector3(0, 1, 0);
+
+
+            return false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            bool a = NextTarget();
+
+            if (a == true)
+            {
+                DestroyPath();
+                CreatePath();
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+

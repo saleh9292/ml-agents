a7bd4f350a723a88fbb4b1b15308bfd0d3311976
Saleh Alsaleh <saleh.alsaleh@outlook.com>
2021-02-16 15:16:17 +0200

    multiple

--- Train1.bat
+++ Train1.bat
@@ -1,6 +1,6 @@
-mlagents-learn config/saleh/UGV12PathLegsObstacles_config5.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log5 --num-envs=4 --time-scale=5 --force
-mlagents-learn config/saleh/UGV12PathLegsObstacles_config6.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log6 --num-envs=4 --time-scale=5 --force
-mlagents-learn config/saleh/UGV12PathLegsObstacles_config7.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log7 --num-envs=4 --time-scale=5 --force
-mlagents-learn config/saleh/UGV12PathLegsObstacles_config8.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log8 --num-envs=4 --time-scale=5 --force
+mlagents-learn config/saleh/UGV12PathLegsObstacles_config5.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log52 --num-envs=1 --time-scale=5 
+mlagents-learn config/saleh/UGV12PathLegsObstacles_config6.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log62 --num-envs=4 --time-scale=5 
+mlagents-learn config/saleh/UGV12PathLegsObstacles_config7.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log72 --num-envs=1 --time-scale=5 
+mlagents-learn config/saleh/UGV12PathLegsObstacles_config8.yaml --env=D:\UnityProjects\Ready --run-id=UGV12PathLegsObstacles_log82 --num-envs=4 --time-scale=5 
 
 
